de23a702d8420972671f99cff4733382bef90574
jayed talukder <mdmahim924214@gmail.com>
2021-09-04 14:26:49 +0600

    complite

--- c.js
+++ c.js
@@ -1,12 +1,35 @@
-let getsearchValue = () =>{
+let cityName = () =>{
     let value = document.getElementById("search-fild");
     let get_value = value.value;
-    console.log(get_value);
-    get_url();
+    value.value = "";
+    return get_value;
 };
-
+let api_key = `100a6b05396087284ca9a3f964e66a55`
 let get_url = () =>{
-    fetch("https://samples.openweathermap.org/data/2.5/forecast?id=524901&appid=b1b15e88fa797225412429c1c50c122a1")
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName()}&appid=${api_key}&units=metric`)
     .then(res => res.json())
-    .then(data = console.log(data));
+    .then(data => displayContant(data));
+}
+let getid =(id,name)=>{
+    let innercity = document.getElementById(id);
+    innercity.innerText = name;
+
+}
+
+let displayContant = (temps) =>{
+    let cityname = temps.name;
+    if (cityname === undefined ){
+        getid("city","No City Found")
+        getid("temperature",00);
+        getid("info","None");
+    }
+    else{
+        getid("city",cityname);
+        getid("temperature",temps.main.temp);
+        getid("info",temps.weather[0].main);
+        let url = `http://openweathermap.org/img/wn/${temps.weather[0].icon}@2x.png`;
+        let icon = document.getElementById("img");
+        icon.setAttribute('src',url);
+
+    }
 }
